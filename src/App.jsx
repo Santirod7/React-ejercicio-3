dbf3c3f9ea11f63248f1e338bc4ed196b30c432e
Santiago Rodriguez <santiara010@gmail.com>
2024-09-11 15:56:58 -0300

    agregue el state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Titulo from './components/Titulo'
+
 
 function App() {
 
   return (
-   <Titulo></Titulo>
+   <Titulo goodDay={'good day'}></Titulo>
   )
 }
 
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,8 +1,14 @@
 
-const Titulo = () => {
+import { useState } from "react";
+const Titulo = ({goodDay}) => {
+    const [boton,setButton] = useState('!')
+    const mostrar = () => {
+    setButton('. Each day is a present!')
+}
     return (
         <section>
-            <h1>¿Cuanto salio argentina?</h1>
+            <h1 className="color">Hello world, {goodDay}{boton} </h1>
+            <button onClick={mostrar}>Click aqui</button>
         </section>
     );
 };
